817931d7ae054183f47bf6f547e0d4df3310f743
Shane Wall <shane.wall@gmail.com>
2025-12-21 00:02:10 +1100

    chore: align tonic stack and deny rules

--- crates/capsule-registry/build.rs
+++ crates/capsule-registry/build.rs
@@ -31,6 +31,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .compile(&["proto/registry_raft.proto"], &["proto"])?;
+        .compile_protos(&["proto/registry_raft.proto"], &["proto"])?;
     Ok(())
 }
